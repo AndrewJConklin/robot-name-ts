64bdadd8d010338522a6f84c654d6cf8fc280ab2
aconkli9 <aconkli9@ford.com>
2022-05-11 11:07:11 -0400

    Add solution

--- robot-name.ts
+++ robot-name.ts
@@ -1,24 +1,52 @@
-export class Robot {
+interface IRobot {
   name: string;
-  constructor() {
-    this.name = nameGenerator();
-  }
-  resetName() {
-    this.name = nameGenerator();
-  }
-  releaseNames() {
-    this.name = "";
-  }
+  resetName: () => void;
 }
 
-function nameGenerator(): string {
-  const char = `ABCDEFGHIJKLMNOPQRSTUVWXYZ`;
-  function randomchar() {
-    return char[Math.floor(Math.random() * char.length)];
+export class Robot implements IRobot {
+  name: string;
+  static usedNames = new Set<string>();
+  static releaseNames() {
+    this.usedNames.clear();
   }
-  function randomnum() {
+
+  constructor() {
+    this.name = this.uniqueRandomName;
+  }
+
+  resetName() {
+    this.name = this.uniqueRandomName;
+  }
+
+  private get uniqueRandomName() {
+    let newName;
+    do {
+      newName = this.randomName;
+    } while (Robot.usedNames.has(newName));
+    Robot.usedNames.add(newName);
+    return newName;
+  }
+
+  private get randomName() {
+    return `${this.twoRandomLetters}${this.randomThreeDigits90}`;
+  }
+  private get randomLetter() {
+    const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const randomNumber = Math.floor(Math.random() * 26);
+    return letters[randomNumber];
+  }
+
+  private get twoRandomLetters() {
+    const letter1 = this.randomLetter;
+    const letter2 = this.randomLetter;
+    return `${letter1}${letter2}`;
+  }
+
+  private get randomThreeDigits90() {
+    return `${this.RandomNumber0to9}${this.RandomNumber0to9}${this.RandomNumber0to9}`
+  }
+
+  private get RandomNumber0to9() {
     return Math.floor(Math.random() * 10);
   }
-  const name = `${randomchar()}${randomchar()}${randomnum()}${randomnum()}${randomnum()}`;
-  return name;
 }
